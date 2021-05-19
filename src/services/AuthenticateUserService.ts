7420958d10e90de28ba1cd13bfe4a0eac6a732c4
Marcos Lucas <marcosvaldeni@gmail.com>
2021-05-19 18:01:14 +0100

    authentication routes

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/passward combination.');
     }
 
-    const token = sign({}, '95c80ec31d741b4c022ce022e717e226', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn
     });
 
     return {
