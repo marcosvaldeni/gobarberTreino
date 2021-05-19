0336d9a0f520f10c3edae1f6352c546d9e997d4b
Marcos Lucas <marcosvaldeni@gmail.com>
2021-05-19 11:10:33 +0100

    genereting token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/passward combination.');
     }
 
+    const token = sign({}, '95c80ec31d741b4c022ce022e717e226', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token
     };
   }
 }
