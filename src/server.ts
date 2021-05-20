469e5a200394af66d2773513c73c851ee3f49553
Marcos Lucas <marcosvaldeni@gmail.com>
2021-05-20 12:00:36 +0100

    serving static file

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hi' });
-});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
