f49709c324aa5e57203ef3fb43ffb2ed6f25d88e
Marcos Lucas <marcosvaldeni@gmail.com>
2021-05-28 20:55:11 +0100

    creating the first test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,11 +1,19 @@
-// import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: new Date(),
+      provider_id: '12312313213'
+    });
+
+    expect(appointment).toHaveProperty('id');
   });
 
-  it('should not be able to create two appointments on the same', () => {
-    expect(1 + 2).toBe(3);
-  });
 });
