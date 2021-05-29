f821c9ceeb70c2699e7ef63b82097d83acd2bcb6
Marcos Lucas <marcosvaldeni@gmail.com>
2021-05-29 11:11:18 +0100

    appointments test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 
@@ -15,6 +18,30 @@ describe('CreateAppointment', () => {
     });
 
     expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12312313213');
   });
+
+  it('should not be able to create two appointment at the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12312313213'
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12312313213'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+  });
+  
 
 });
