a7f2a0f804d1c3c5cdedaa079aeda5ce8f773be5
Marcos Lucas <marcosvaldeni@gmail.com>
2021-07-08 19:10:48 +0100

    emails templante

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,11 +1,19 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { injectable, inject } from 'tsyringe';
 
 import IMailProvider from "../models/IMailProvider";
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider
+  ) {
     nodemailer.createTestAccount().then(account => {
 
       const  transporter = nodemailer.createTransport({
@@ -23,12 +31,18 @@ export default class EtherealMailProvider implements IMailProvider {
 
   }
   
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({ to, from, subject, templateData }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Team GoBarber <equipe@gobarber.com',
-      to,
-      subject: 'Recovering password',
-      text: body,
+      from: {
+        name: from?.name || 'GoBarber Team',
+        address: from?.email || 'gobarberteam@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
