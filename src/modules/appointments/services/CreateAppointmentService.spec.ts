b780157166637166279d913327e44178effe0f70
Marcos Lucas <marcosvaldeni@gmail.com>
2021-07-11 15:54:39 +0100

    Deleting old appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -19,6 +19,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '1231',
       provider_id: '12312313213'
     });
 
@@ -31,12 +32,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '1231',
       provider_id: '12312313213'
     });
 
     expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '1231',
         provider_id: '12312313213'
       }),
     ).rejects.toBeInstanceOf(AppError);
--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -18,11 +18,13 @@ describe('ListProvidersDayAvailability', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: '1231',
       date: new Date(2020, 4, 20, 8, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: '1231',
       date: new Date(2020, 4, 20, 10, 0, 0),
     });
 
