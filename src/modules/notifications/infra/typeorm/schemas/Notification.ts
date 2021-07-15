619084aed2437731e956a470cf83c9ca95e436ca
Marcos Lucas <marcosvaldeni@gmail.com>
2021-07-15 19:15:23 +0100

    notification structure

---
+++ src/modules/notifications/infra/typeorm/schemas/Notification.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, ObjectID, ObjectIdColumn, UpdateDateColumn } from 'typeorm';
+
+class Notification {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  content: string;
+
+  @Column('uuid')
+  recipient_id: string;
+
+  @Column({ default: false })
+  read: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Notification;
