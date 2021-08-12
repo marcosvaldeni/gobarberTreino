e293083959d947ba6ae562b8f93e8a0abd127459
Marcos Lucas <marcosvaldeni@gmail.com>
2021-08-12 16:15:14 +0100

    Express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,15 +7,17 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
-import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import reteLimiter from './middlewares/rateLimiter';
+import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
-app.use(cors());
 
+app.use(reteLimiter);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadFolder));
 app.use(routes);
