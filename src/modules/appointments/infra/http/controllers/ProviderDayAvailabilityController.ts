7b609f91a13ac3d658f780b0838bfff8ed293a10
Marcos Lucas <marcosvaldeni@gmail.com>
2021-08-12 17:03:26 +0100

    Using Query Params

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -6,19 +6,15 @@ import ListProviderDayAvailabilityService  from '@modules/appointments/services/
 export default class ProviderDayAvailabilityController {
   public async index(req: Request, res: Response): Promise<Response> {
     const { provider_id } = req.params;
-    const {
-      day,
-      month,
-      year
-    } = req.body;
+    const { day, month, year } = req.query;
  
     const listProviderDayAvailabilityService = container.resolve(ListProviderDayAvailabilityService);
   
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
   
     return res.json(availability);
